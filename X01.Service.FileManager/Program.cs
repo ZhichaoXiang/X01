96f3f19431844c0e8e13610fd80f668fffc3dcc6
zhichaoxiang <zhichaoxiang86@gmail.com>
2022-03-10 22:44:03 +0100

    x01

---
+++ X01.Service.FileManager/Program.cs
@@ -0,0 +1,10 @@
+using X01.Service.FileManager;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
