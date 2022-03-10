89f0f0666ccfd3eeea8f7a7075b1be22d0bc5666
ZhichaoXiang <zhichaoxiang86@gmail.com>
2022-03-10 15:13:07 +0100

    Update CancelableSource.cs

--- Core/CancelableSource.cs
+++ Core/CancelableSource.cs
@@ -1,1 +1,11 @@
 
+namespace X01{
+
+public static class CancelableSourceExt{
+public static IEnumerable<TItem> MonitorCancellation(this CancelableSource cs,IEnumerable<TItem> enumerable ){
+return enumerable.select(x=>   { 
+cs.throwifcancellationrequested
+return x;});
+}
+}
+}
