ca7aa9e5a2d25fea3ba7a2b1d40544edffca6d39
zhichaoxiang <zhichaoxiang86@gmail.com>
2022-03-10 22:16:03 +0100

    core

--- Core/BindableSource.cs
+++ Core/BindableSource.cs
@@ -1,1 +1,16 @@
+using System.ComponentModel;
 
+namespace X01
+{
+    public class BindableSource : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventHandler(propertyName));
+        }
+        public void SetProperty()
+        {
+        }
+    }
+}
--- Core/CancelableSource.cs
+++ Core/CancelableSource.cs
@@ -1,11 +1,42 @@
 
-namespace X01{
+using System.Threading;
 
-public static class CancelableSourceExt{
-public static IEnumerable<TItem> MonitorCancellation(this CancelableSource cs,IEnumerable<TItem> enumerable ){
-return enumerable.select(x=>   { 
-cs.throwifcancellationrequested
-return x;});
-}
-}
+namespace X01
+{
+
+    public class CancelableSource<TObject>
+    {
+        readonly TObject _obj;
+        readonly CancellationToken? _ct;
+        public TObject Obj
+        {
+            get
+            {
+                _ct?.ThrowIfCancellationRequested();
+                return (_obj);
+            }
+        }
+        public CancelableSource(TObject obj, CancellationToken? ct)
+        {
+            _obj = obj;
+            _ct = ct;
+        }
+        public CancelableSource(TObject obj) : this(obj, null)
+        {
+        }
+        //public static implicit operator TObject(CancelableSource<TObject> c) => c.Obj;
+        // public static explicit operator CancelableSource<TObject>(TObject b, CancellationToken? ct) => new CancelableSource<TObject>(b, ct);
+    }
+
+    public static class CancelableSourceExt
+    {
+        public static IEnumerable<TItem> MonitorCancellation(this CancelableSource cs, IEnumerable<TItem> enumerable)
+        {
+            return enumerable.select(x =>
+            {
+                cs.throwifcancellationrequested
+            return x;
+            });
+        }
+    }
 }
